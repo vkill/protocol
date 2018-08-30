84ab778cccb55003be51bdd4f0db9ae54cb340ea
jjc <910455361@qq.com>
2018-08-30 12:31:57 +0800

    上传新项目

---
+++ operation/src/main/java/com/space/protocol/ProtocolApplication.java
@@ -0,0 +1,12 @@
+package com.space.protocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtocolApplication.class, args);
+    }
+}
