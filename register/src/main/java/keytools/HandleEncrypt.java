fe5ed913968d14237cebb37ef9ff97f8adc6f420
jjc <910455361@qq.com>
2018-10-12 19:18:55 +0800

    ces加密

--- register/src/main/java/keytools/HandleEncrypt.java
+++ register/src/main/java/keytools/HandleEncrypt.java
@@ -1,6 +1,13 @@
 package keytools;
 
- /**
+import com.mysql.cj.util.Base64Decoder;
+import com.space.register.entity.DeviceEntity;
+import params.CesJson;
+import util.HttpTools.HttpTools;
+
+import java.util.Base64;
+
+/**
  *           .]]]]]]`.            .]]]]`           .]]]]].            .,]]]]]`        .]]]]`
  *         ,@@@@@@@@@@^    @@@@./@@@@@@@@@^    =@@@@@@@@@@@@.      ]@@@@@@@@@@@^   ,@@@@@@@@@@`
  *        .@@@@`    .[`    @@@@@@@[`..[@@@@@   =@/`    .\@@@@    ,@@@@@[.    ,[.  /@@@/.  .\@@@\
@@ -22,9 +29,39 @@ package keytools;
  **/
 public class HandleEncrypt {
 
-    private static final String HoST = "crc.kitejoy.com";
+    private static final String HOST = "http://crc.kitejoy.com";
 
     private static final String PORT = "11600";
+
+     /**
+      * Ces的加密
+      * @param type POST还是GET
+      * @param params json字符串
+      * @return
+      */
+    public static byte[] getCes(String type, String params) {
+        String url = HOST + ":" + PORT + "/DouYin/Ces";
+        Base64.Encoder encoder = Base64.getEncoder();
+        String base64params = encoder.encodeToString(params.getBytes());
+
+//        System.out.println(base64params);
+
+        // 发送请求
+        String post = HttpTools.post(url, "data=" + base64params + "&type=1");
+
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] decode = decoder.decode(post);
+        return decode;
+    }
+
+    public static void main(String[] args) {
+        DeviceEntity deviceEntity = new DeviceEntity();
+        String s = CesJson.jsonConstruct(deviceEntity);
+        byte[] posts = getCes("POST", s);
+        String print = new String(posts);
+        System.out.println(print);
+
+    }
 
 
 }
